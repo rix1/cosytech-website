56a3a14612e75e4584a7359e1b9f94bcb8ec82c4
Andreas Royrvik <andreas@royrvik.org>
2015-03-19 00:21:57 +0100

    Added wsgi config

--- website/website/wsgi.py
+++ website/website/wsgi.py
@@ -10,5 +10,16 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
+# Virtualenv
+import site
+import sys
+PROJECT_ROOT = '/var/www/cosytech.net/cosytech-website/website/'
+site_packages = os.path.join(PROJECT_ROOT, '../env/lib/python2.7/site-packages/')
+site.addsitedir(os.path.abspath(site_packages))
+sys.path.insert(0, site_packages)
+sys.path.insert(0, PROJECT_ROOT)
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
